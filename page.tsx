96baee106459ea2f5d179de290d1f476502b4117
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-28 11:12:37 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+const page = () => {
+  return (
+    <div className="flex justify-center flex-col items-center h-screen ">
+      <div className="rounded-full overflow-hidden">
+        <Image src={"/picture.jpg"} alt="my picture" width={300} height={300} />
+      </div>
+      <div className="m-5">
+        <h2>This is My Image</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
